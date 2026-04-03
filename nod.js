c418972bf6834216d29c94d27ec9554294cbaa9f
mohamedabdelsattar <mohamedabd33310@gmail.com>
2026-04-03 16:46:12 +0200

    ,,mm

--- nod.js
+++ nod.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Docker App this app the best of the world 🚀");
+  res.send("Hello from Docker App this app the best of the world and we get the better version of the app  🚀");
 });
 
 app.listen(3000, '0.0.0.0', () => {console.log("App running on port 3000")});
